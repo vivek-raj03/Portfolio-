00f258985546f084a0cf3a68a4f0a53e4ee43784
vive-raj <vivekraj03023@gmail.com>
2024-03-15 22:09:00 +0530

    Homepage half done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
 
+
 function Home() {
+  
   return (
     <>
     <Navbar />
@@ -9,8 +11,8 @@ function Home() {
       <div className="text-white">
        <div className=" mt-48 md:mt-60 md:ml-32 ">
        <p className=" text-5xl md:text-7xl w-full text-center md:text-left">Hi There</p>
-       <p className='text-5xl md:text-7xl text-center md:text-left'>I'm Vivek <span className="text-orange-500">Raj</span></p>
-       <p className='md:text-4xl text-3xl text-center md:text-left'>I am a Web <span className="text-orange-500">Developer</span></p>
+       <p className='text-5xl md:text-7xl text-center md:text-left mt-3'>I'm Vivek <span className="text-orange-500">Raj</span></p>
+       <p className='md:text-4xl text-3xl text-center md:text-left ml-1 mt-3'>I am a Web <span className="text-orange-500  ">Developer</span></p>
        </div>
       </div>
     </div>
