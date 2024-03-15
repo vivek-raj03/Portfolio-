edd404710cd0e8d937eefb3586c26ebc12737f09
vive-raj <vivekraj03023@gmail.com>
2024-03-15 21:20:18 +0530

    home2

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,10 +7,10 @@ function Home() {
     <Navbar />
     <div className= "bg-black h-screen w-full z-[-99] text-white fixed">
       <div className="text-white">
-       <div className=" mt-28 md:mt-60 md:ml-32 ">
-       <p className=" text-4xl md:text-7xl w-full text-center sm:text-left">Hi There</p>
-       <p className='text-4xl md:text-7xl text-center sm:text-left'>I'm Vivek <span className="text-orange-500">Raj</span></p>
-       <p className='md:text-4xl text-center sm:text-left'>I am a Web <span className="text-orange-500">Developer</span></p>
+       <div className=" mt-48 md:mt-60 md:ml-32 ">
+       <p className=" text-5xl md:text-7xl w-full text-center md:text-left">Hi There</p>
+       <p className='text-5xl md:text-7xl text-center md:text-left'>I'm Vivek <span className="text-orange-500">Raj</span></p>
+       <p className='md:text-4xl text-3xl text-center md:text-left'>I am a Web <span className="text-orange-500">Developer</span></p>
        </div>
       </div>
     </div>
