9f10aa243e14d3d50c06d4a3c3da672ff39f8547
vive-raj <vivekraj03023@gmail.com>
2024-03-19 16:17:31 +0530

    Project done

--- src/Pcard.jsx
+++ src/Pcard.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import { motion } from 'framer-motion';
 import './Border.css'
 
-function Pcard() {
+function Pcard(props) {
+    const { title, description,address } = props.data;
     const[isOpen,setIsOpen] = useState(false);
     return (
         <>
-            <motion.div transition={{layout:{duration:2}}}layout onClick={()=>setIsOpen(!isOpen)} className='bg-black w-96 h-96 text-white flex flex-col justify-center items-center border-2 border-slate-50 rounded-xl'>
-                <motion.h2 layout="position" className='text-3xl font-roberto mb-4 text-orange-500 text-shadow'>Technical Skills</motion.h2>
+            <motion.div transition={{layout:{duration:2}}}layout onClick={()=>setIsOpen(!isOpen)} className='bg-black w-80 h-80 text-white flex flex-col justify-center items-center border-2 border-slate-50 rounded-xl'>
+                <motion.h2 layout="position" className='text-3xl font-roberto mb-4 text-orange-500 text-shadow'>{title}</motion.h2>
                 {isOpen && (
-                <p className='text-center'>Proficient in<span className='text-orange-600'>Java</span>  with expertise in C, C++, and Python, showcasing versatile programming skills across multiple languages. adept at crafting robust solutions and leveraging diverse toolsets to tackle complex challenges.</p>
+                    <>
+                <p className='text-center'>{description}</p>
+                <a href={address} target='blank'>Click here</a>
+                </>
                 )}
             </motion.div>
         </>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Pcard from './Pcard'
+import React from 'react';
+import Navbar from './Navbar';
+import Pcard from './Pcard';
+
 function Projects() {
+  const cardData = [
+    {
+        title: "Portfolio",
+        description: "Developed a versatile portfolio with React, Tailwind CSS, and JavaScript, showcasing proficiency in frontend technologies.",
+        address: "http://vivek-raj-portfolio.vercel.app"
+    },
+    {
+        title: "Another Title",
+        description: "Another Description",
+        address: "http://vivek-raj-portfolio.vercel.app"
+    }
+];
   return (
     <>
-    <Navbar></Navbar>
-    <div className='w-full h-[100vh] bg-black 
-    mt-[75px] md:mt-19'>
-      <Pcard></Pcard>
-    </div>
+    <Navbar />
+      <div className="bg-black w-full flex flex-row justify-center flex-wrap min-h-screen  text-white ">
+        <div className='flex flex-wrap mt-[200px] justify-center md:space-x-2 md:space-y-0 space-y-2'>
+          {cardData.map((data,index)=>(
+            <Pcard key={index} data={data}></Pcard>
+           ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
